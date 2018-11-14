3002a1e3ce6eb5524eab2d63a2c8114a3f6d9a22
alex <alexmir2015@yandex.ru>
2018-11-14 20:49:32 +0300

    Make repository pageable

--- src/main/java/com/web/portal/repository/GameRepository.java
+++ src/main/java/com/web/portal/repository/GameRepository.java
@@ -1,9 +1,12 @@
 package com.web.portal.repository;
 
 import com.web.portal.models.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
+    Page<Game> findAll(Pageable pageable);
 }
