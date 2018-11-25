9be5c712f269c3dc3968ed3e5b7fb742d0884a99
Yowzah <putnikovsemen@gmail.com>
2018-11-25 18:38:23 +0300

    this version works

--- src/main/java/com/web/portal/controllers/GameController.java
+++ src/main/java/com/web/portal/controllers/GameController.java
@@ -2,8 +2,10 @@ package com.web.portal.controllers;
 
 import com.web.portal.models.Company;
 import com.web.portal.models.Game;
+import com.web.portal.models.Pager;
 import com.web.portal.repository.CompanyRepository;
 import com.web.portal.repository.GameRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,11 @@ import java.util.Optional;
 @Controller
 public class GameController {
 
+    private static final int BUTTONS_TO_SHOW = 3;
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 5;
+    private static final int[] PAGE_SIZES = { 5, 10};
+
     private GameRepository gameRepository;
 
     private CompanyRepository companyRepository;
@@ -26,9 +33,24 @@ public class GameController {
     }
 
     @GetMapping(value = {"/", "/index"})
-    public String index(Model model) {
-        List<Game> games = gameRepository.findAll(PageRequest.of(0, 5)).getContent();
-        model.addAttribute("games", games);
+    public String index(Model model, @RequestParam("pageSize") Optional<Integer> pageSize,
+                        @RequestParam("page") Optional<Integer> page) {
+
+        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+
+        Page<Game> gameList = gameRepository.findAll(new PageRequest(evalPage, evalPageSize));
+
+        Pager pager = new Pager(gameList.getTotalPages(), gameList.getNumber(),BUTTONS_TO_SHOW);
+
+        model.addAttribute("gameList", gameList);
+        // evaluate page size
+        model.addAttribute("selectedPageSize", evalPageSize);
+        // add page sizes
+        model.addAttribute("pageSizes", PAGE_SIZES);
+        // add pager
+        model.addAttribute("pager", pager);
+
         return "homePage";
     }
 
--- src/main/java/com/web/portal/repository/GameRepository.java
+++ src/main/java/com/web/portal/repository/GameRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends PagingAndSortingRepository<Game, Long> {
     //Page<Game> findAll(Pageable pageable);
 }
