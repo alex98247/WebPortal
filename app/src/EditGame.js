29b314c8123b825027b118ea05b7d534cabd8dac
alex <alexmir2015@yandex.ru>
2018-11-29 15:05:15 +0300

    Edit Genre

--- app/src/EditGame.js
+++ app/src/EditGame.js
@@ -3,7 +3,7 @@ import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
 
 class EditGame extends Component {
 
-    state = {game: {}};
+    state = {game: {}, genres: []};
 
     constructor(props) {
         super(props);
@@ -13,7 +13,13 @@ class EditGame extends Component {
 
 
     async componentDidMount() {
-        this.setState({game: this.props.location.state.game});
+        const response = await fetch('/api/genre');
+        const body = await response.json();
+
+        this.setState({
+            game: this.props.location.state.game,
+            genres: body
+        });
     }
 
     handleChange(event) {
@@ -41,21 +47,30 @@ class EditGame extends Component {
     }
 
     render() {
-        const {game} = this.state;
+        const {game, genres} = this.state;
+
+        const genreList = genres.map(genre =>  <option>{genre}</option>);
 
         return (
             <Form onSubmit={this.handleSubmit}>
                 <FormGroup>
                     <Label for="name">Name</Label>
-                    <Input type="text" name="name" id="name" value={game.name || ''} onChange={this.handleChange} />
+                    <Input type="text" name="name" id="name" value={game.name || ''} onChange={this.handleChange}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="description">description</Label>
-                    <Input type="text" name="description" id="description" value={game.description || ''} onChange={this.handleChange} />
+                    <Input type="text" name="description" id="description" value={game.description || ''}
+                           onChange={this.handleChange}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="year">year</Label>
-                    <Input type="text" name="year" id="year" value={game.year || ''} onChange={this.handleChange} />
+                    <Input type="text" name="year" id="year" value={game.year || ''} onChange={this.handleChange}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="genre">year</Label>
+                    <Input type="select" name="genre" id="genre" value={game.genre || ''} onChange={this.handleChange}>
+                        {genreList}
+                    </Input>
                 </FormGroup>
                 <FormGroup>
                     <Button color="primary" type="submit">Save</Button>
